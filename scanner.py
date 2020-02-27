b326237cfc2d0405465d6ce47b74fa9f5f24637a
Night Guard <1072171610dk@gmail.com>
2020-02-27 05:24:36 -0800

    add new function scan_subnet

--- scanner.py
+++ scanner.py
@@ -1,36 +1,39 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 
 """
-Scan the network and capture the packet
-"""
-
-import sys
-
-from IPy import IP as IP_ADD
-from scapy.layers.inet import IP, ICMP
-from scapy.sendrecv import sr1
-
-ipv4_segment = sys.argv[1]
-ip_list = [str(i) for i in IP_ADD(ipv4_segment)]
-
-print("IP_LIST: " + str(ip_list))
-
-for ip in ip_list:
-    p = sr1(IP(dst=ip)/ICMP(), timeout=2)
-    if p:
-        print("[+]host {} is alive".format(ip))
-    else:
-        print("[-]host {} is not alive".format(ip))
-
-=======
-'''
 @Author: k
 @Date: 2020-02-12 21:31:33
 @LastEditTime : 2020-02-12 22:07:39
 @LastEditors  : k
 @Description: packet scanner
 @FilePath: /NightGuard/scanner.py
-'''
->>>>>>> test git rebase
 
+Scan the network and capture the packet
+"""
+
+from scapy.layers.inet import IP, ICMP
+from scapy.sendrecv import sr1
+
+
+def scan_subnet(ip_list):
+    """
+    scan network segment
+    :param ip_list:list
+    :return: res:dict
+    """
+    res = {
+        'alive': [],
+        'dead': []
+    }
+
+    if isinstance(ip_list, list) and ip_list is not None:
+        for ip in ip_list:
+            p = sr1(IP(dst=ip)/ICMP(), timeout=2)
+            if p:
+                res['alive'].append(ip)
+            else:
+                res['dead'].append(ip)
+        return res
+    # 傳入參數有誤
+    else:
+        return None
